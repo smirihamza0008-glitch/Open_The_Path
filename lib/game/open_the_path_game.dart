1c8993e8e26f9c455fdb0649ae229bfe2a20327e
smirihamza0008-glitch <smirihamza0008@gmail.com>
2026-06-05 10:09:21 +0100

    Update open_the_path_game.dart

--- lib/game/open_the_path_game.dart
+++ lib/game/open_the_path_game.dart
@@ -1,18 +1,18 @@
 import 'package:flame/game.dart';
-import 'package:flame/input.dart';
+import 'package:flame/events.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'player_component.dart';
 import 'finish_line_component.dart';
 import 'tap_obstacle_component.dart';
-import 'slide_obstacle_component.dart'; // تعديل التسمية البرمجية الصحيحة هنا
+import 'slide_obstacle_component.dart';
 
-class OpenThePathGame extends FlameGame with HasCollisionDetection, HasTappables, HasDraggables {
+class OpenThePathGame extends FlameGame with HasCollisionDetection, HasComponentRef {
   late PlayerComponent player;
   late FinishLineComponent finishLine;
 
-  final Function() onGameOver; // المسمى المعتمد في شاشة التجميع الخاصة بك
+  final Function() onGameOver;
   final Function() onLevelComplete;
 
   OpenThePathGame({
@@ -42,13 +42,11 @@ class OpenThePathGame extends FlameGame with HasCollisionDetection, HasTappables
     );
     add(finishLine);
 
-    // عائق النقر
     add(TapObstacleComponent(
       position: Vector2(size.x * 0.3, size.y / 2 - 40),
       size: Vector2(50, 80),
     ));
 
-    // عائق السحب - تم إصلاح الاسم هنا ليطابق الملف تماماً
     add(SlideObstacleComponent(
       position: Vector2(size.x * 0.6, size.y / 2 - 50),
       size: Vector2(40, 100),
