bf33456baaef6dc8adc07d6adf61005839499500
smirihamza0008-glitch <smirihamza0008@gmail.com>
2026-06-05 09:31:49 +0100

    Update open_the_path_game.dart

--- lib/game/open_the_path_game.dart
+++ lib/game/open_the_path_game.dart
@@ -1,56 +1,57 @@
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'player_component.dart';
 import 'finish_line_component.dart';
 import 'tap_obstacle_component.dart';
-import 'slide_obstacle_component.dart';
+import 'slice_obstacle_component.dart';
 
-class OpenThePathGame extends FlameGame with HasCollisionDetection, TapDetector, HasDraggablesBridge {
+class OpenThePathGame extends FlameGame with HasCollisionDetection, TapDetector, DragDetector {
   late PlayerComponent player;
   late FinishLineComponent finishLine;
-  
-  final Function() onGameOver;
+
+  final Function() gameOver;
   final Function() onLevelComplete;
 
   OpenThePathGame({
-    required this.onGameOver,
+    required this.gameOver,
     required this.onLevelComplete,
   });
 
   @override
   Future<void> onLoad() async {
-    super.onLoad();
+    await super.onLoad();
     startLevel();
   }
 
   void startLevel() {
     removeAll(children);
 
-    // 1. إضافة اللاعب في اليسار
+    // إضافة اللاعب في اليسار
     player = PlayerComponent(
       position: Vector2(50, size.y / 2 - 20),
       size: Vector2(40, 40),
-      playerColor: const Color(0xffff007f),
+      playerColor: const Color(0xfffff007f),
     );
     add(player);
 
-    // 2. إضافة خط النهاية في أقصى اليمين
+    // إضافة خط النهاية في أقصى اليمين
     finishLine = FinishLineComponent(
       position: Vector2(size.x - 60, 0),
       size: Vector2(20, size.y),
     );
     add(finishLine);
 
-    // 3. ابتكار وتوليد المعيقات في منتصف مسار اللاعب هندسياً
-    // معيق النقر المتعدد الموضع في الثلث الأول من المرحلة
+    // ابتكار وتوليد العقبات في منتصف مسار اللاعب هندسياً
+    // عائق النقر المتعدد الموجود في الثلث الأول من المرحلة
     add(TapObstacleComponent(
       position: Vector2(size.x * 0.3, size.y / 2 - 40),
       size: Vector2(50, 80),
     ));
 
-    // معيق السحب الموضع في الثلث الثاني من المرحلة
+    // عائق السحب الموجود في الثلث الثاني من المرحلة
     add(SlideObstacleComponent(
       position: Vector2(size.x * 0.6, size.y / 2 - 50),
       size: Vector2(40, 100),
@@ -70,6 +71,6 @@ class OpenThePathGame extends FlameGame with HasCollisionDetection, TapDetector,
 
   void triggerGameOver() {
     player.speed = 0;
-    onGameOver();
+    gameOver();
   }
 }
